08d3aac854a5297d22a67e91f788856b65137deb
Yiwan Zhou <mail@yiwan.me>
2025-04-12 12:40:37 +0000

    refactor

--- packages/doc/src/preview/components.tsx
+++ packages/doc/src/preview/components.tsx
@@ -65,7 +65,7 @@ import {
   Tooltip,
   Tree,
   TreeItem
-} from '../../../ui/src/components'
+} from 'vecui'
 import { Card } from './card'
 import { componentsClassName } from './components.css'
 
