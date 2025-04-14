a2d443559477f111c3c55200c113924bfa8d3da3
Yiwan Zhou <mail@yiwan.me>
2025-04-14 12:37:52 +0000

    refactor: remove astro doc

--- packages/doc/src/components/theme.tsx
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx'
-import { type PropsWithChildren, useState } from 'react'
-import { semanticClassName, themeLightClassName } from 'vecui'
-
-export function Theme(props: PropsWithChildren) {
-  const [themeClassName] = useState(themeLightClassName)
-
-  return <div className={clsx(themeClassName, semanticClassName)}>{props.children}</div>
-}
