daa34de1c62221fe09ce32c43b4da8c48175b0e9
Yiwan Zhou <mail@yiwan.me>
2025-05-16 12:28:16 +0000

    wip

--- packages/vecui/vite.config.ts
+++ packages/vecui/vite.config.ts
@@ -1,7 +1,17 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import reactPlugin from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
-  plugins: [reactPlugin(), vanillaExtractPlugin()]
+  plugins: [reactPlugin(), vanillaExtractPlugin()],
+  build: {
+    lib: {
+      entry: resolve(dirname(fileURLToPath(import.meta.url)), './src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+      cssFileName: 'index'
+    }
+  }
 })
