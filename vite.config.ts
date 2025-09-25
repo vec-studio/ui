044d4817475d9f2f6a45bc939e94e383b99bc3a6
Yiwan Zhou <mail@yiwan.me>
2025-09-25 17:21:04 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import dtsPlugin from 'vite-plugin-dts'
 import { externalizeDeps as externalizeDepsPlugin } from 'vite-plugin-externalize-deps'
 
 export default defineConfig({
-  plugins: [reactPlugin(), vanillaExtractPlugin(), externalizeDepsPlugin(), dtsPlugin()],
+  plugins: [reactPlugin(), vanillaExtractPlugin(), externalizeDepsPlugin(), dtsPlugin({ outDir: 'dist/types' })],
   build: {
     lib: {
       entry: resolve(dirname(fileURLToPath(import.meta.url)), 'src/index.ts'),
