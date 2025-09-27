fa1f920b6827ebbab3f10e70e59614ef93443a5c
Yiwan Zhou <mail@yiwan.me>
2025-09-27 14:33:12 +0000

    wip

--- src/theme/dark.css.ts
+++ src/theme/dark.css.ts
@@ -4,6 +4,14 @@ import openProps from 'open-props'
 import { themeContract } from './contract.css'
 import { tokenKeys, type Tokens } from './token.css'
 
-const themeDarkTokens = lodash.pick(openProps, tokenKeys) as Tokens
+const themeDarkTokens = lodash.reduce(
+  tokenKeys,
+  (prev, curr) => {
+    // @ts-ignore
+    prev[curr] = openProps[curr]
+    return prev
+  },
+  {}
+) as { [K in keyof Tokens]: string }
 
 export const themeDarkClassName = createTheme(themeContract, themeDarkTokens)
--- src/theme/light.css.ts
+++ src/theme/light.css.ts
@@ -4,6 +4,14 @@ import openProps from 'open-props'
 import { themeContract } from './contract.css'
 import { tokenKeys, type Tokens } from './token.css'
 
-const themeLightTokens = lodash.pick(openProps, tokenKeys) as Tokens
+const themeLightTokens = lodash.reduce(
+  tokenKeys,
+  (prev, curr) => {
+    // @ts-ignore
+    prev[curr] = openProps[curr]
+    return prev
+  },
+  {}
+) as { [K in keyof Tokens]: string }
 
 export const themeLightClassName = createTheme(themeContract, themeLightTokens)
