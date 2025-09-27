9b61810f008de8fe2146d6574ffa05696008448a
Yiwan Zhou <mail@yiwan.me>
2025-09-27 14:46:21 +0000

    wip

--- src/components/select-value.tsx
+++ src/components/select-value.tsx
@@ -2,7 +2,7 @@ import { SelectValue as RACSelectValue, type SelectValueProps as RACSelectValueP
 import { selectValueClassName } from './select-value.css'
 import { cn } from './utils'
 
-export type SelectValueProps<T> = RACSelectValueProps<T>
+export type SelectValueProps<T extends object> = RACSelectValueProps<T>
 
 export function SelectValue<T extends object>(props: SelectValueProps<T>) {
   return <RACSelectValue {...props} className={cn(props.className, selectValueClassName)} />
